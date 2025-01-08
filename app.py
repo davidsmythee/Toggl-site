672f9fb0bba67cae83f90d5eedb47eed524b917a
David Smythe <davidwsmythe22@gmail.com>
2025-01-08 00:09:27 -0500

    added requirements

--- app.py
+++ app.py
@@ -9,9 +9,11 @@ app = Flask(__name__)
 def index():
     print("Serving the index page...")  # Debug print
     return render_template('index.html')
-
+    
 if __name__ == '__main__':
-    print("Running Flask app...")  # Debug print
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
+
 
 
